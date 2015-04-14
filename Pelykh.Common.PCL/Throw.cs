60ee4488ddf904892fb5fca5224e0475ed7f71aa
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-04-14 13:25:11 +0300

    Another version of argument check

--- Pelykh.Common.PCL/Throw.cs
+++ Pelykh.Common.PCL/Throw.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Pelykh.Common
 {
     public static class Throw
     {
-        //public static void IfNull<T>(Expression<Func<T>> expression)
-        //    where T : class
-        //{
-            
-        //}
+        public static void IfNull<T>(Expression<Func<T>> expression)
+            where T : class
+        {
+            var body = (MemberExpression)expression.Body;
+            var name = body.Member.Name;
+            var value = ((FieldInfo)body.Member).GetValue(((ConstantExpression)body.Expression).Value);
+
+            if (value != null)
+                return;
+
+            if (name != null)
+                throw new ArgumentNullException(name);
+
+            throw new ArgumentNullException();
+        }
     }
 }
