097e6440580fdddbe27a78e1b9166fec9cf82f28
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-06-17 18:53:33 +0300

    Pelykh.Common.Net.PCL

---
+++ Pelykh.Common.Net.PCL/WebRequestExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Pelykh.Common.Net
+{
+    public static class WebRequestExtensions
+    {
+        public static Task<WebResponse> GetResponseAsync(this WebRequest webRequest)
+        {
+            webRequest.ThrowIfNull("this");
+
+            return Task.Factory.StartNew(() => Task<WebResponse>.Factory.FromAsync(
+                webRequest.BeginGetResponse,
+                webRequest.EndGetResponse,
+                null).Result);
+        }
+
+        public static Task<Stream> Stream(this WebRequest webRequest)
+        {
+            return Task.Factory.StartNew(() => Task<Stream>.Factory.FromAsync(
+                webRequest.BeginGetRequestStream,
+                webRequest.EndGetRequestStream,
+                null).Result);
+        }
+    }
+}
